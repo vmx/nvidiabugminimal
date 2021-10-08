bdf69aa861574042a2376ba918d21df3bcd961e1
Volker Mische <volker.mische@gmail.com>
2021-10-08 14:16:53 +0200

    feat: use opencl3 directly
    Also commit the kernels that work and down't work

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,41 @@
-use rust_gpu_tools::{opencl, Device};
+use opencl3::{
+    command_queue::CommandQueue,
+    context::Context,
+    device::{Device, CL_DEVICE_TYPE_GPU},
+    kernel::{self, ExecuteKernel},
+    platform,
+    program::Program,
+};
 
-static SOURCE: &str = include_str!("kernel.cl");
+//static SOURCE: &str = include_str!("kernel.cl");
+//static SPIRV: &[u8] = include_bytes!("../intel_working.spv");
+static SPIRV: &[u8] = include_bytes!("../intel_broken.spv");
 
 pub fn main() {
     let aa: u32 = 972342711;
     let bb: u32 = 1698717651;
 
-    let device = *Device::all().first().expect("Cannot get a default device");
-    let opencl_device = device.opencl_device().unwrap();
-    let program = opencl::Program::from_opencl(opencl_device, SOURCE).unwrap();
+    let platform = *platform::get_platforms().unwrap().first().unwrap();
+    let raw_device = *platform
+        .get_devices(CL_DEVICE_TYPE_GPU)
+        .unwrap()
+        .first()
+        .unwrap();
+    let device = Device::new(raw_device);
+    let context = Context::from_device(&device).unwrap();
+    //let program = Program::create_and_build_from_source(&context, SOURCE, "").unwrap();
+    let mut program = Program::create_from_il(&context, SPIRV).unwrap();
+    program.build(&[raw_device], "").unwrap();
 
-    let kernel = program.create_kernel("minimal_two_vars", 1, 1).unwrap();
-    kernel.arg(&aa).arg(&bb).run().unwrap();
+
+    let kernels = kernel::create_program_kernels(&program).unwrap();
+    let kernel = kernels.first().unwrap();
+    let queue = CommandQueue::create_with_properties(&context, raw_device, 0, 0).unwrap();
+    ExecuteKernel::new(&kernel)
+        .set_arg(&aa)
+        .set_arg(&bb)
+        .set_local_work_size(1)
+        .set_global_work_size(1)
+        .enqueue_nd_range(&queue)
+        .unwrap();
 }
